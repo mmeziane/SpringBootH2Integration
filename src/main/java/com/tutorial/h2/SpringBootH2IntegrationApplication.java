1b1f6c8fddf80a66d2c5b98fe59ea0d6b5e1105a
mmeziane <mmeziane@users.noreply.github.com>
2019-03-28 03:16:00 +0100

    first Commit.

---
+++ src/main/java/com/tutorial/h2/SpringBootH2IntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2IntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2IntegrationApplication.class, args);
+	}
+
+}
